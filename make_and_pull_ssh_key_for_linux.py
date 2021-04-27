87e6bb1cbb39031c62e0acba1cb34b8a4b63e80c
daniel himes <dhimes@gmail.com>
2021-04-27 13:06:05 -0400

    assumes no existing SSH key, creates that key, then copies it to clipboard and prints it

---
+++ make_and_pull_ssh_key_for_linux.py
@@ -0,0 +1,50 @@
+import netmiko
+from getpass import getpass
+from netmiko import ConnectHandler
+from pprint import pprint
+import pyperclip
+
+
+user = 'dhimes'
+email = 'dhimes@gmail.com'
+
+
+def pull_key():
+    command = f'ssh-keygen -t ed25519 -C "{email}"'
+    commands = [
+        "eval `ssh-agent -s`",
+        "ssh-add ~/.ssh/id_ed25519",
+    ]
+    connection = make_conn(user, email)
+    output = connection.send_command(command, expect_string=':')
+    output = connection.send_command("", expect_string=':')
+    output = connection.send_command("", expect_string=':')
+    output = connection.send_command("", expect_string=':')
+    for command in commands:
+        output = connection.send_command(command)
+    output = connection.send_command('more ~/.ssh/id_ed25519.pub')
+    pyperclip.copy(output)
+    print(output)
+
+
+def make_conn(user, email):
+    user_input = input(f'user: [{user}]:')
+    if user_input != '':
+        user = user_input
+    user_input = input(f'email: [{email}]:')
+    if user_input != '':
+        email = user_input
+    server = input('server name:')
+    password = getpass()
+
+    conn_data = {
+        'device_type': 'linux',
+        # 'verbose': True,
+        'ip': server,
+        'username': user,
+        'password': password,
+    }
+    return ConnectHandler(**conn_data)
+
+
+pull_key()
